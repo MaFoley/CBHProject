098224517b2fab9cb9d600aeb87983e289e8044b
mafoley1 <mafoley1@linux-2.cbhp.ua.edu>
2014-04-14 22:02:39 -0500

    Succesfully created first Loading file.
    Other ones to come.

--- CreateState.cpp
+++ CreateState.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include<cstring>
-#include<cstdlib>
-#include<fstream>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
 
 using namespace std;
 const int CODELENGTH = 2;
@@ -24,4 +24,63 @@ int main()
     ofstream outputFile;
 
     inputFile.open(INFILE);
+    
+    if(!inputFile)
+    {
+        cerr << "Problem openeing input file: " << INFILE << endl;
+        return 1;
+    }
 
+    outputFile.open(OUTFILE);
+
+    if(!outputFile)
+    {
+        cerr << "Problem openeing output file: " << OUTFILE << endl;
+        inputFile.close();
+        return 2;
+    }
+
+    numValid = 0;
+    numInvalid = 0;
+
+    memset(outBuffer, ' ', sizeof(outBuffer));
+
+    while(true)
+    {
+        memset(inBuffer, ' ', sizeof(inBuffer));
+        inputFile.getline(inBuffer, sizeof(inBuffer));
+
+        if(inputFile.eof())
+            break;
+
+        strncpy(charCode, inBuffer, CODELENGTH);
+        charCode[CODELENGTH] = '\0';
+        intCode = atoi(charCode);
+        
+        if(intCode < MINCODE || intCode > MAXCODE)
+        {
+            ++numInvalid;
+            continue;
+        }
+
+        int copyLength = strlen(inBuffer) - CODELENGTH < DATALENGTH
+            ? strlen(inBuffer) - CODELENGTH : DATALENGTH;
+        strncpy(outBuffer + ((intCode - MINCODE) * DATALENGTH),
+                inBuffer + CODELENGTH,
+                copyLength);
+        ++numValid;
+    }
+    outputFile.seekp(0);
+    outputFile.write((char *) &numValid, sizeof(numValid));
+
+    outputFile.seekp(DATALENGTH);
+    outputFile.write(outBuffer, sizeof(outBuffer));
+
+    inputFile.close();
+    outputFile.close();
+
+    cout << "Number of valid records written to " << OUTFILE << ": " << numValid << endl;
+    cout << "Number of invalid not written: " << numInvalid << endl;
+     
+    return 0;
+}
