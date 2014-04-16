8d17373a382401903ab7435d6bb5761bd049b548
mafoley1 <mafoley1@linux-2.cbhp.ua.edu>
2014-04-15 23:54:55 -0500

    Successfully reading from .db files!

--- opt1.cpp
+++ opt1.cpp
@@ -1,30 +1,47 @@
 #include <iostream>
-#include <string>
+#include <cstring>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
 
-const int DATALENGTH = 20;
-const int CODELENGTH = 2;
+const int DATALENGTH = 22;
 const int ABBRLENGTH = 2;
+const int NAMELENGTH = 20;
 
 int main()
 {
     //Read from direct access file into inBuffer;
+    ifstream inputFile;
+    int numRecords;
+    char inBuffer[DATALENGTH + 1];
+    char charAbbr[ABBRLENGTH + 1];
+    char charName[NAMELENGTH + 1];
 
-    string inBuffer;
 
-    string strCode;
-    string strAbbr;
-    string dataName;
+    inputFile.open("state.db");
 
-    inBuffer = "09DCDistrict of Columbia";
-    strCode = inBuffer[0:1];
-    strAbbr = inBuffer[2:3];
-    dataName = inBuffer[4:23];
+    inputFile.seekg(0);
+    inputFile.read((char *) &numRecords, sizeof(numRecords));
 
-    char lineBuffer[DATALENGTH + 3];
-    strncpy(lineBuffer, strCode.c_str(), CODELENGTH);
-    strncpy(lineBuffer + CODELENGTH, strAbbr.c_str(), ABBRLENGTH);
-    strncpy(lineBuffer + CODELENGTH + ABBRLENGTH, dataName.c_str(), DATALENGTH - CODELENGTH - ABBRLENGTH);
+    for(int counter = 1; counter <= numRecords; counter ++)
+    {
+        inputFile.seekg(DATALENGTH * counter); //Counter acts as the record number
+        inputFile.read(inBuffer, DATALENGTH);
+        inBuffer[DATALENGTH] = '\0';
+
+        strncpy(charAbbr, inBuffer, ABBRLENGTH);
+        strncpy(charName, inBuffer, NAMELENGTH);
+        charAbbr[ABBRLENGTH] = '\0';
+        charName[NAMELENGTH] = '\0';
+
+        cout.fill('0');
+        cout.width(2);
+        cout << counter;
+        cout.fill(' ');
+        cout << ' ' << charAbbr << ' ' << charName << '\n';
+    }
+    inputFile.close();
+
 
     return 0;
 }
