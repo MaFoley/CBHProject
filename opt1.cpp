57162289a25387606e1b10c4bf3b09746f816417
mafoley1 <mafoley1@linux-2.cbhp.ua.edu>
2014-04-15 23:17:53 -0500

    Testing branches from remote

--- opt1.cpp
+++ opt1.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 void option1()
 {
     cout << "User selected option 1.\n";
 }
 
+void option1()
+{
+    Read from direct access file into inBuffer;
+
+    string strCode;
+    string strAbbr;
+    string dataName;
+
+    strCode = inBuffer[0:1]
+}
+/*
+ * XX AB STATE NAME(20)
+ */
 
